4c36678d294f4f65ecf3ce9b088fe0a26033d957
Ben Wunderlich <35752279+aGenericHuman@users.noreply.github.com>
2018-07-07 20:02:52 -0700

    init file
    used for verifying input as numbers

---
+++ num_inp.py
@@ -0,0 +1,14 @@
+def numeric_ensure(question):
+    frustrated = False
+    while True:
+        try:
+            if frustrated:
+                varia = int(input("I SAID, " + question))
+                break
+            else:
+                varia = int(input(question))
+                break
+        except ValueError:
+            frustrated = True
+            continue
+    return varia
