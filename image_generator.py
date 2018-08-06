29905414c6b26456680867c7321a60a0cd5aced3
Ben Wunderlich <35752279+aGenericHuman@users.noreply.github.com>
2018-08-06 16:43:46 -0700

    added better multiplication
    used regular expressions(re.search) and text replacement to make it easier to use multiplication and exponents that are more consistent with normal use

--- image_generator.py
+++ image_generator.py
@@ -2,6 +2,7 @@ from num_inp import numeric_ensure
 from PIL import Image
 import numpy
 import math
+import re
 
 def the_equation(root_formula, x):  # I might make a thing so that you can turn a string into a formula
     return eval(root_formula)
@@ -14,6 +15,8 @@ def add_pixel(the_formula, pix, line_to_print_on, pixel_states, the_array):
 
 
 def make_image(formula, zero_works, y_height, x_width):
+    print("\nmaking an image for the equation:", formula, "\n")
+
     PIXEL_STATES = 255
     pic_array = numpy.empty(shape=(0, x_width))
     if zero_works:
@@ -25,23 +28,46 @@ def make_image(formula, zero_works, y_height, x_width):
             curr_line = pic_array.size // x_width
             current_pix, pic_array = add_pixel(formula, i, curr_line, PIXEL_STATES, pic_array)
 
-    print("the shape of the array is:", pic_array.shape)
-    print("the size of the array is:", pic_array.size)
+    '''print("the shape of the array is:", pic_array.shape)
+    print("the size of the array is:", pic_array.size)'''
 
-    le_image = Image.fromarray(pic_array, mode="L")
-    le_image.show()
-    return le_image
+    the_image = Image.fromarray(pic_array, mode="L")
+    the_image.show()
+    return the_image
 
 
 def error_message():
-    print("that does not work, "
-          "remember it must be structured like 2*x not 2x")
+    print("that does not work")
+
+
+def make_executable(formula):
+    formula = formula.replace(" ", "") # removes spaces
+
+    end_mult_pattern = r"[0-9]x"
+    end_match = re.search(end_mult_pattern, formula)
+    while end_match:
+        if end_match:
+            split = end_match.end()-1
+            formula = formula[:split] + "*" + formula[split:]
+        end_match = re.search(end_mult_pattern, formula)
+
+    strt_mult_patt = r"x[0-9]"
+    strt_match = re.search(strt_mult_patt, formula)
+    while strt_match:
+        if strt_match:
+            split = strt_match.start()+1
+            formula = "{}*{}".format(formula[:split], formula[split:])
+        strt_match = re.search(strt_mult_patt, formula)
+    formula = formula.replace("^", "**")
+    formula = formula.replace("", "")
+    return formula
 
 
 def test_eval():
     while True:
         try:
             thing_to_test = input("formula?")
+            thing_to_test = make_executable(thing_to_test)
             x = 0
             eval(thing_to_test)
         except SyntaxError:
@@ -60,7 +86,7 @@ def help_commands():
 'resize' -- change the dimensions of the image
    'new' -- change the formula used to create the image
   'quit' -- quits the program
-  'save' -- saves the last image
+  'save' -- saves the last image created
     """)
 
 
@@ -73,7 +99,7 @@ def save_image(image):
         file_name = input("name of file?")
     file_name += ".PNG"
     image.save(file_name)
-    print(file_name, "has been saved")
+    print("\n{} has been saved\n".format(file_name))
 
 
 def main():
@@ -100,10 +126,9 @@ def main():
         elif usr_choice == "quit":
             continue
         else:
-            print("that is not a command I am familiar with, type 'help' to"
-                  " see a list of valid commands")
+            print("\nthat is not a command I am familiar with, type 'help' to"
+                  " see a list of valid commands\n")
 
 
 if __name__ == "__main__":
     main()
-    
